9d6944c60d3f1cea5f65436995f4c6b5df280b06
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2022-05-08 09:48:07 -0300

    Delete item, delete todos

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -5,18 +5,25 @@ import { TaskList } from './../../model/task-list';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
-
-  public taskList: Array<TaskList>=[
-    { task: "Minha nova Task", checked: true },
-    { task: "Minha nova Task 2", checked: false}
+  public taskList: Array<TaskList> = [
+    { task: 'Minha nova Task', checked: true },
+    { task: 'Minha nova Task 2', checked: false },
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1);
   }
-
+  public deleteAllTaskList() {
+    const confirm = window.confirm('Você deseja realmente deletar tudo?');
+    if (confirm) {
+      this.taskList = [];
+    }
+  }
 }
